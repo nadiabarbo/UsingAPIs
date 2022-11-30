fb344b9064d8d29e66f2f2e9aca02b2994e53c1b
nadiabarbo <112417252+nadiabarbo@users.noreply.github.com>
2022-11-30 12:37:01 -0500

    Update Open_Jupyter_gis.bat

--- Open_Jupyter_gis.bat
+++ Open_Jupyter_gis.bat
@@ -1,4 +1,4 @@
-@set the_env=gis
+@set the_env=gis2
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "%the_env%"
 call "%localappdata%\ESRI\conda\envs\%the_env%\scripts\jupyter-notebook.exe" %cd% 
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
